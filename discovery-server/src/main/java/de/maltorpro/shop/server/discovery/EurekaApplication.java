4f560c4abf1f99a3d5ed1e90f895122649acc371
Malte Basse <maltebasse@gmail.com>
2018-05-11 02:33:52 +0200

    http2 support

--- discovery-server/src/main/java/de/maltorpro/shop/server/discovery/EurekaApplication.java
+++ discovery-server/src/main/java/de/maltorpro/shop/server/discovery/EurekaApplication.java
@@ -11,22 +11,23 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 import de.maltorpro.shop.infrastructure.utils.SSLUtils;
 
-
 @SpringBootApplication
 @EnableEurekaServer
 @EnableDiscoveryClient
 public class EurekaApplication {
 
-	public static void main(String[] args) throws KeyManagementException, NoSuchAlgorithmException {
-		
-		String certificateCheck =  System.getProperty("certificateCheck");
-		
-		if(StringUtils.equals(certificateCheck, "false") || StringUtils.equals(certificateCheck, "0")) {
-			
-			SSLUtils.turnOffSslChecking();
-		}
-		
-		SpringApplication.run(EurekaApplication.class, args);
-	}
+    public static void main(String[] args)
+            throws KeyManagementException, NoSuchAlgorithmException {
+
+        String certificateCheck = System.getProperty("certificateCheck");
+
+        if (StringUtils.equals(certificateCheck, "false")
+                || StringUtils.equals(certificateCheck, "0")) {
+
+            SSLUtils.turnOffSslChecking();
+        }
+
+        SpringApplication.run(EurekaApplication.class, args);
+    }
 
 }
